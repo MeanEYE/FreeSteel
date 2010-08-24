8d79b56ec9eb5ef260bcc6a517e2ff50a925f699
Goran Rakic <grakic@devbase.net>
2010-08-24 14:52:53 +0200

    Feature to extract personal X.509 certificates

--- freesteel.py
+++ freesteel.py
@@ -33,29 +33,47 @@ def usage():
 FreeSteel %s - read data of the Serbian national eID card
 Copyright (c) 2010 Goran Rakic <grakic@devbase.net>.
 
-A cross-platform and free Python script built on top of pyscard and PC/SC.
+A cross-platform and a free Python script built on top of pyscard and PC/SC.
 Released under the GNU LGPL license version 3  or (at your option) any later.
 
 Usage: %s [option]... where option can be one of:
 
-  -p, --photo[=FILENAME]    Copy the photo of the smart card. If the filename
-                            is not specified, <JMBG>.jpg will be used
+  -r --report[=FILE]       Create the PDF report with personal data and photo
+                           If the filename is not specified, <JMBG>.pdf will be
+                           used. (THE FEATURE IS NOT IMPLEMENTED!)
 
-  -r, --report[=FILENAME]   Create the PDF report with all data including the
-                            photo. If the filename is not specified, <JMBG>.pdf
-                            will be used (THE FEATURE IS NOT IMPLEMENTED!)
-
-  -d, --dump=DIRECTORY      Dump EF binary data into the directory
-  -v, --verbose             Output sent and received data to stderr
-
-      --help                Display this message
-      --version             Output version number
+     --silent              Do not output data on screen
+                           Use this to just extract files to a given filename
+                           without reading other data from the eID card.
 
 
-Report bugs to grakic@devbase.net. See the README file for more information
-how you can help test and improve FreeSteel.
+  Extract files options
+
+  -p --photo[=FILE]        Copy the photo of the smart card
+                           Default filename is <JMBG>.jpg where JMBG is personal
+                           JMBG number read from the eID card.
+
+  -q --qualified[=FILE]    Extract qualified public personal X.509 certificate
+                           Default filename is <JMBG>.jpg where JMBG is personal
+                           JMBG number read from the eID card.
+
+  -s --standard[=FILE]     Extract standard public personal X.509 certificate
+                           Default filename is <JMBG>.jpg where JMBG is personal
+                           JMBG number read from the eID card.
+
+
+  Debug and informational options
+
+  -d --dump=DIRECTORY      Dump binary EF data into the directory
+  -v --verbose             Output sent and received data to stderr
+
+     --help                Display this message
+     --version             Output version number
+
+
+Report bugs to grakic@devbase.net. See the README file for more information how
+you can help test and improve FreeSteel.
 """ % (VERSION, sys.argv[0])
-
 
 def create_context():
   hresult, hcontext = SCardEstablishContext(SCARD_SCOPE_USER)
@@ -87,11 +105,21 @@ def card_disconnect(hcard):
   if hresult != SCARD_S_SUCCESS:
     raise Exception('Failed to disconnect: ' + SCardGetErrorMessage(hresult))
 
-# APDU commands for Serbian eID
+# APDU commands for the Serbian eID
 cmd = {
   'GET_DATA_0101'   : [0x00, 0xCA, 0x01, 0x01, 0x20], # > GET_DATA_0101
-  'SELECT_FILE_PATH': [0x00, 0xA4, 0x08, 0x00],       # > SELECT_FILE_PATH, file_path
+  'SELECT_FILE_PATH': [0x00, 0xA4, 0x08, 0x00],       # > SELECT_FILE_PATH, file_path_len, file_path bytes...
   'READ_BINARY'     : [0x00, 0xB0]                    # > READ_BINARY, 2 bytes byte_offset, byte_length
+}
+
+# EF files available on the Serbian eID
+files = {
+  'DOCUMENT'  : [0x0F, 0x02],   # Document data
+  'PERSONAL'  : [0x0F, 0x03],   # Personal data
+  'RESIDENCE' : [0x0F, 0x04],   # Place of residence, variable length
+  'PHOTO'     : [0x0F, 0x06],   # Personal photo in JPEG format
+  'QUALIFIED' : [0x0F, 0x08],   # Public X.509 certificate for qualified signing
+  'STANDARD'  : [0x0F, 0x10]    # Public X.509 certificate for standard signing
 }
 
 def card_transmit(hcard, dwActiveProtocol, *data):
@@ -106,21 +134,23 @@ def card_transmit(hcard, dwActiveProtocol, *data):
 
   if debug: print "<", smartcard.util.toHexString(response)
 
-  # Check for 0x90 0x00 (all OK) confirmation
+  # Check for 0x90 0x00 (all OK) confirmation in response
   check = response[len(response)-2:]
   if check != [0x90, 0x00]:
     raise Exception('Request error: Returned ' + smartcard.util.toHexString(check))
 
   return response[:-2]
 
-def eid_read_ef(hcard, dwActiveProtocol, ef_path):
+def eid_read_ef(hcard, dwActiveProtocol, ef_path, dump_directory = None):
   # select, ignore security info reply (read 1byte + 0x90 0x00)
-  card_transmit(hcard, dwActiveProtocol, cmd['SELECT_FILE_PATH'], ef_path, [0x01])
+  card_transmit(hcard, dwActiveProtocol, cmd['SELECT_FILE_PATH'], [len(ef_path)], ef_path, [0x01])
 
   # read first 6 bytes to get ef len as 16bit LE integer at 4B offset
-  r = card_transmit(hcard, dwActiveProtocol, cmd['READ_BINARY'], [0x00, 0x00, 0x06])
-  #print r
-  ef_len = r[4]+r[5]*256 + 6
+  header = card_transmit(hcard, dwActiveProtocol, cmd['READ_BINARY'], [0x00, 0x00, 0x06])
+  if len(header) < 6:
+    raise Exception('Request error: Could not read header from EF path ' + smartcard.util.toHexString(ef_path))
+  
+  ef_len = header[4]+header[5]*256 + 6
 
   data = []
   ef_off = 6
@@ -130,9 +160,9 @@ def eid_read_ef(hcard, dwActiveProtocol, ef_path):
     data.extend(card_transmit(hcard, dwActiveProtocol, cmd['READ_BINARY'], [ef_off>>8, ef_off&0xff, limit]))
     ef_off += limit
 
-  if dump:
+  if dump_directory:
     filename = "ef_%s.bin" % smartcard.util.toHexString(ef_path).replace(' ', '_')
-    dump_bin_string(os.path.join(dump, filename), data)
+    dump_bin_string(os.path.join(dump_directory, filename), header+data)
 
   return data
 
@@ -148,6 +178,12 @@ def eid_split_fields(data):
     fdata.append(data[i+4:i+4+length])
     i += 4+length
   return fdata, flabels
+
+def eid_extract_file(hcard, dwActiveProtocol, ef_path, filename, dump_directory = None):
+  data = eid_read_ef(hcard, dwActiveProtocol, ef_path, dump_directory)
+  f = open(filename, "wb+")
+  f.write(b2a(data[4:]))
+  f.close()
 
 def cli_select_widget(list, title):
   c = 0
@@ -174,28 +210,33 @@ def dump_bin_string(filename, data):
   f.write(''.join([chr(b) for b in data]))
   f.close()
 
-dump = None
-debug = False
-photo = False;  photo_filename = None
+dump_directory = None
+debug = False; silent = False
 report = False; report_filename = None
+photo = False;  photo_filename = None
+qualified = False; qualified_filename = None
+standard = False; standard_filename = None
 
 def read_options():
-  global dump, debug, photo, photo_filename, report, report_filename
+  global dump_directory, debug, silent, report, report_filename
+  global photo, photo_filename, qualified, qualified_filename, standard, standard_filame
 
   try:
-    opts, args = getopt.getopt(sys.argv[1:], "prd:v", ["photo", "report", "dump=", "verbose", "help", "version"])
+    opts, args = getopt.getopt(sys.argv[1:], "rpqsd:v", ["report", "photo", "qualified", "standard", "silent", "dump=", "verbose", "help", "version"])
   except getopt.GetoptError, err:
     print >> sys.stderr, str(err)
     usage()
     sys.exit(2)
 
   for o, a in opts:
-    if o == "-v":
+    if o in ("-v", "--verbose"):
       debug = True
+    elif o == "--silent":
+      silent = True
     elif o in ("-d", "--dump"):
-      dump = a
-      if not os.path.exists(dump):
-        os.makedirs(dump)
+      dump_directory = a
+      if not os.path.exists(dump_directory):
+        os.makedirs(dump_directory)
     elif o in ("-h", "--help"):
       usage()
       sys.exit()
@@ -205,11 +246,21 @@ def read_options():
     elif o in ("-p", "--photo"):
       photo = True
       photo_filename = a
+    elif o in ("-q", "--qualified"):
+      qualified = True
+      qualified_filename = a
+    elif o in ("-s", "--standard"):
+      standard = True
+      standard_filename = a
     elif o in ("-r", "--report"):
       report = True
       report_filename = a
       print >> sys.stderr, "PDF report feature is not implemented"
       sys.exit(3)
+
+  if silent and debug:
+    print >> sys.stderr, "I do not know yet how to be both silent and verbose"
+    sys.exit(2)
 
 def b2a(bytes):
   return smartcard.util.toASCIIString(bytes)
@@ -229,59 +280,84 @@ def main():
 
     try:
 
-      # Get card's Answer To Reset (not required)
-      hresult, n, n, n, atr = SCardStatus(hcard)
-      if hresult != SCARD_S_SUCCESS:
-        raise Exception('Failed to get ATR: ' + SCardGetErrorMessage(hresult))
-      print "ATR            :", smartcard.util.toHexString(atr)
-      
-      # Start communication, send GET DATA header
-      r = card_transmit(hcard, dwActiveProtocol, cmd['GET_DATA_0101'])
-      header = smartcard.util.toHexString(r, smartcard.util.PACK)
-      print "Header field   :", header
-      print "Printed number :", " "*17, header[18:32]
+      if debug:
+        # Get card's Answer To Reset (not required)
+        hresult, n, n, n, atr = SCardStatus(hcard)
+        if hresult != SCARD_S_SUCCESS:
+          raise Exception('Failed to get ATR: ' + SCardGetErrorMessage(hresult))
+        print "ATR            :", smartcard.util.toHexString(atr)
 
-      # Select ef_02_0f_02
-      data = eid_read_ef(hcard, dwActiveProtocol, [0x02, 0x0F, 0x02])
-      fdata, flabels = eid_split_fields(data)
-      print "eID number     :", b2a(fdata[0])
-      print "Issued         :", b2a(fdata[3])
-      print "Valid          :", b2a(fdata[4])
-      print "Issuer         :", b2u(fdata[5]), b2a(fdata[6])
+      # Header and document data
+      if not silent or report:
+        # Start communication, send GET DATA header
+        data = card_transmit(hcard, dwActiveProtocol, cmd['GET_DATA_0101'])
+        data = smartcard.util.toHexString(data, smartcard.util.PACK)
+        print "Header field   :", data
+        print "Printed number :", " "*17, data[18:32]
 
-      # Select ef_02_0f_03
-      data = eid_read_ef(hcard, dwActiveProtocol, [0x02, 0x0F, 0x03])
-      fdata, flabels = eid_split_fields(data)
-      jmbg = b2a(fdata[0])
-      print "JMBG           :", jmbg
-      print "Family name    :", b2u(fdata[1])
-      print "First name     :", b2u(fdata[2])
-      print "Middle name    :", b2u(fdata[3])
-      print "Gender         :", b2u(fdata[4])
-      print "Place od birth :", "%s, %s, %s, %s" % (b2u(fdata[5]), b2u(fdata[6]), b2u(fdata[7]), b2u(fdata[9]))
-      print "Date of birth  :", b2a(fdata[8])
+        # Select EF path 0f 02
+        data = eid_read_ef(hcard, dwActiveProtocol, files['DOCUMENT'], dump_directory)
+        fdata, flabels = eid_split_fields(data)
+        print "eID number     :", b2a(fdata[0])
+        print "Issued         :", b2a(fdata[3])
+        print "Valid          :", b2a(fdata[4])
+        print "Issuer         :", b2u(fdata[5]), b2a(fdata[6])
 
-      # Select ef_02_0f_04
-      data = eid_read_ef(hcard, dwActiveProtocol, [0x02, 0x0F, 0x04])
-      fdata, flabels = eid_split_fields(data)
+      # Personal data
+      read_personal = not silent or report
+      # We still need jmbg from the personal details
+      if (photo and not photo_filename) or (qualified and not qualified_filename) or (standard and not qualified_standard):
+        read_personal = True
 
-      residence  = "%s, %s, %s" % (b2u(fdata[1]), b2u(fdata[2]), b2u(fdata[0]))
-      if len(fdata) > 3:
-        residence = string.join([b2u(d) for d in fdata[3:]], ', ')+"\n"+" "*17+residence
-      print "Residence      :", residence
+      if read_personal:
+        # Select EF path 0f 03
+        data = eid_read_ef(hcard, dwActiveProtocol, files['PERSONAL'], dump_directory)
+        fdata, flabels = eid_split_fields(data)
+        jmbg = b2a(fdata[0])
+        if not silent:
+          print "JMBG           :", jmbg
+          print "Family name    :", b2u(fdata[1])
+          print "First name     :", b2u(fdata[2])
+          print "Middle name    :", b2u(fdata[3])
+          print "Gender         :", b2u(fdata[4])
+          print "Place od birth :", "%s, %s, %s, %s" % (b2u(fdata[5]), b2u(fdata[6]), b2u(fdata[7]), b2u(fdata[9]))
+          print "Date of birth  :", b2a(fdata[8])
 
-      if photo or dump:
-        # Select ef_02_0f_06
-        data = eid_read_ef(hcard, dwActiveProtocol, [0x02, 0x0F, 0x06])
+
+      # Place of residence data
+      if not silent or report:
+        # Select EF path 0f 04
+        data = eid_read_ef(hcard, dwActiveProtocol, files['RESIDENCE'], dump_directory)
+        fdata, flabels = eid_split_fields(data)
+
+        residence  = "%s, %s, %s" % (b2u(fdata[1]), b2u(fdata[2]), b2u(fdata[0]))
+        if len(fdata) > 3:
+          residence = string.join([b2u(d) for d in fdata[3:]], ', ')+"\n"+" "*17+residence
+        print "Residence      :", residence
+
+
+      # Dump files without extracting and saving individual files
+      if dump_directory:
+        for n in (files['PHOTO'], files['QUALIFIED'], files['STANDARD']):
+          eid_read_ef(hcard, dwActiveProtocol, n, dump_directory)
 
       if photo:
-        if not photo_filename:
-          filename = jmbg+".jpg"
-        else:
-          filename = photo_filename
-        f = open(filename, "wb+")
-        f.write(b2a(data[4:]))
-        f.close()
+        # Select EF path 0f 06
+        if not photo_filename: filename = jmbg+".jpg"
+        else: filename = photo_filename
+        eid_extract_file(hcard, dwActiveProtocol, files['PHOTO'], filename, None)
+
+      if qualified:
+        # Select EF path 0f 08
+        if not qualified_filename: filename = jmbg+"_qualified.cer"
+        else: filename = qualified_filename
+        eid_extract_file(hcard, dwActiveProtocol, files['QUALIFIED'], filename, None)
+
+      if standard:
+        # Select EF path 0f 10
+        if not standard_filename: filename = jmbg+"_standard.cer"
+        else: filename = standard_filename
+        eid_extract_file(hcard, dwActiveProtocol, files['STANDARD'], filename, None)
 
     finally:
       card_disconnect(hcard)
